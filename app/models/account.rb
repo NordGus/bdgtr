b26f3873dcab1cdd468aebb5c6e7537002ec2f55
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-20 19:35:53 +0100

    refactoring Account model validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,5 @@ class Account < ApplicationRecord
   has_many :outgoing_transactions, class_name: "Transaction", foreign_key: :from_id, dependent: :destroy
   has_many :incoming_transactions, class_name: "Transaction", foreign_key: :to_id, dependent: :destroy
 
-  validates_presence_of :name
-  validates_length_of :name, minimum: 5
+  validates :name, presence: true, length: { minimum: 5 }
 end
