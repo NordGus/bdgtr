fc33d06dcbdc88ff781d7b0ff95b1d61a9425c43
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-20 18:43:00 +0100

    adding model validations to Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ApplicationRecord
   has_many :outgoing_transactions, class_name: "Transaction", foreign_key: :from_id, dependent: :destroy
   has_many :incoming_transactions, class_name: "Transaction", foreign_key: :to_id, dependent: :destroy
+
+  validates_presence_of :name
+  validates_length_of :name, minimum: 5
 end
