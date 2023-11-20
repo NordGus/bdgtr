7170981ff351c904258412ccabbdb7b644c9aaf5
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-20 19:20:23 +0100

    adding Transaction model loop prevention validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :from, class_name: "Account"
   belongs_to :to, class_name: "Account"
+
+  validates_comparison_of :from_id, other_than: :to_id
+  validates_comparison_of :to_id, other_than: :from_id
 end
