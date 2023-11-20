75512e5af00565cbc77771332567aa6b414b2d70
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-20 18:32:25 +0100

    adding account failing tests

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have incoming transactions" do
+    account = accounts(:personal)
+    assert_equal account.incoming_transactions.count, 1, "account doesn't have incoming transactions"
+  end
+
+  test "should have outgoing transactions" do
+    account = accounts(:personal)
+    assert_equal account.outgoing_transactions.count, 1, "account doesn't have incoming transactions"
+  end
+
+  test "name must be present" do
+    account = Account.new
+    assert_not account.valid?, "must be invalid"
+    assert_includes account.errors.messages_for(:name), "can't be blank", "must validate account's name presence"
+  end
+
+  test "name must be at least 5 characters long" do
+    account = Account.new
+    assert_not account.valid?, "must be invalid"
+    assert_includes account.errors.messages_for(:name), "is too short (minimum is 5 characters)", "must validate that account's name length"
+  end
 end
