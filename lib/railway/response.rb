4c17586b826ace7b21651d9412c0a476a1815c8a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-21 02:33:32 +0100

    adding base implementation of Response control flow methods

--- lib/railway/response.rb
+++ lib/railway/response.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Response
+  SUCCESS = :success.freeze
+  FAILURE = :failure.freeze
+
   attr_accessor :type, :args
 
   def initialize(type, *args)
@@ -11,10 +14,22 @@ class Response
   end
 
   def self.success(*args)
-    new(:success, *args)
+    new(SUCCESS, *args)
   end
 
   def self.failure(*args)
-    new(:failure, *args)
+    new(FAILURE, *args)
+  end
+
+  def and_then(&block)
+    return block.call(*args) if type == SUCCESS
+
+    self
+  end
+
+  def if_failed(&block)
+    return block.call(*args) if type == FAILURE
+
+    self
   end
 end
--- test/lib/railway/response_test.rb
+++ test/lib/railway/response_test.rb
@@ -24,4 +24,30 @@ class ResponseTest < ActiveSupport::TestCase
     assert_equal response.type, :failure, "invalid type"
     assert_equal response.args, ["this is a test"], "invalid arguments"
   end
+
+  test "#and_then when success" do
+    response = Response.success("this is a test")
+
+    assert_equal response.and_then{ |*args| Response.new(:test, "this comes from a block") }.type, :test, "bad block execution"
+    assert_equal response.and_then{ |*args| Response.new(:test, "this comes from a block") }.args, ["this comes from a block"], "bad block execution"
+  end
+
+  test "#and_then when failure" do
+    response = Response.failure("this is a test")
+
+    assert_equal response.and_then{ |*args| Response.new(:test, "this comes from a block") }, response, "bad block execution"
+  end
+
+  test "#if_failed when success" do
+    response = Response.success("this is a test")
+
+    assert_equal response.if_failed{ |*args| Response.new(:test, "this comes from a block") }, response, "bad block execution"
+  end
+
+  test "#if_failed when failure" do
+    response = Response.failure("this is a test")
+
+    assert_equal response.if_failed{ |*args| Response.new(:test, "this comes from a block") }.type, :test, "bad block execution"
+    assert_equal response.if_failed{ |*args| Response.new(:test, "this comes from a block") }.args, ["this comes from a block"], "bad block execution"
+  end
 end
