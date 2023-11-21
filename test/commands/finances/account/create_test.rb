2566c42f62ba7adb4b5de254c8cd88bd99175792
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-21 21:41:36 +0100

    implementing Finances::Account::Create command

---
+++ test/commands/finances/account/create_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class Finances::Account::CreateTest < ActiveSupport::TestCase
+  test "validates the presence of the name parameter" do
+    command = ::Finances::Account::Create.new(name: "")
+    response = command.execute
+
+    assert_not response.success?, "must not succeed"
+    assert_equal response.args.first, command, "must return an instance of itself"
+    assert_equal response.args.last, {name: ["can't be blank"]}, "must validate name's presence"
+  end
+
+  test "fails if the account already exists" do
+    command = ::Finances::Account::Create.new(name: accounts(:personal).name)
+    response = command.execute
+
+    assert_not response.success?, "must not succeed"
+    assert_includes response.args,
+                    "Account (#{accounts(:personal).name}) already exists.",
+                    "must failed because there's and account with the same name in the system"
+  end
+
+  test "succeeds to create and new Account" do
+    account_name = "On the bounce"
+    command = ::Finances::Account::Create.new(name: account_name)
+    response = nil
+
+    assert_difference "Account.count", 1, "must create an Account" do
+      response = command.execute
+    end
+
+    assert response&.success?, "must succeed"
+    assert_equal response&.args&.first&.name, account_name, "must create the new Account with the correct name"
+  end
+end
