5d305a5304121871b371c8a0beef051e2ba0ed61
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-23 20:11:58 +0100

    reorganizing code to be correct with documentation and reducing the amount of assertions for models

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,67 +1,123 @@
 require "test_helper"
 
 class TransactionTest < ActiveSupport::TestCase
-  test "should have an origin Account" do
-    transaction = transactions(:personal_expense_0)
-    expected_origin_account = accounts(:personal)
+  class Validation < ActiveSupport::TestCase
+    test "origin account presence" do
+      parameters = {
+        from: nil,
+        to: accounts(:expense),
+        amount: 420.69,
+        issued_at: Date.today
+      }
+      transaction = Transaction.new(parameters)
 
-    assert_equal transaction.from.id, expected_origin_account.id, "transaction doesn't come from expected transaction"
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["can't be blank"],
+                   transaction.errors.messages_for(:from_id),
+                   "must validate origin account is present"
+    end
+
+    test "destination account presence" do
+      parameters = {
+        from: accounts(:personal),
+        to: nil,
+        amount: 420.69,
+        issued_at: Date.today
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["can't be blank"],
+                      transaction.errors.messages_for(:to_id),
+                      "must validate destination account is present"
+    end
+
+    test "destination account must be other than origin account" do
+      parameters = {
+        from: accounts(:personal),
+        to: accounts(:personal),
+        amount: 420.69,
+        issued_at: Date.today
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["must be other than #{accounts(:personal).id}"],
+                   transaction.errors.messages_for(:to_id),
+                   "must validate destination account is other than origin account"
+    end
+
+    test "amount presence" do
+      parameters = {
+        from: accounts(:personal),
+        to: accounts(:expense),
+        amount: nil,
+        issued_at: Date.today
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["can't be blank"],
+                   transaction.errors.messages_for(:amount),
+                   "must validate amount is present"
+    end
+
+    test "amount must be greater than 0" do
+      parameters = {
+        from: accounts(:personal),
+        to: accounts(:expense),
+        amount: -420.69,
+        issued_at: Date.today
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["must be greater than 0"],
+                   transaction.errors.messages_for(:amount),
+                   "must validate that amount greater than 0"
+    end
+
+    test "issued_at presence" do
+      parameters = {
+        from: accounts(:personal),
+        to: accounts(:expense),
+        amount: 420.69,
+        issued_at: nil
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["can't be blank"],
+                   transaction.errors.messages_for(:issued_at),
+                   "must validate transaction's issued_at presence"
+    end
+
+    test "executed_at must be equal or later than issued_at when is present" do
+      parameters = {
+        from: accounts(:personal),
+        to: accounts(:expense),
+        amount: 420.69,
+        issued_at: Date.today,
+        executed_at: Date.yesterday
+      }
+      transaction = Transaction.new(parameters)
+
+      assert_not transaction.valid?, "must be invalid"
+      assert_equal ["must be greater than or equal to #{parameters[:issued_at]}"],
+                   transaction.errors.messages_for(:executed_at),
+                   "must validate transaction's executed_at is equal or later than issued_at"
+    end
+  end
+
+  test "should have an origin Account" do
+    assert_equal accounts(:personal).id,
+                 transactions(:personal_expense_0).from.id,
+                 "transaction doesn't come from expected transaction"
   end
 
   test "should have a destination Account" do
-    transaction = transactions(:personal_expense_0)
-    expected_destination_account = accounts(:expense)
-
-    assert_equal transaction.to.id, expected_destination_account.id, "transaction doesn't go tp expected transaction"
-  end
-
-  test "origin account must be other than destination account" do
-    account = accounts(:personal)
-    transaction = Transaction.new(from: account, to: account)
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:from_id), "must be other than #{account.id}", "must validate transaction's destination account must be other than its origin account"
-  end
-
-  test "destination account must be other than origin account" do
-    account = accounts(:personal)
-    transaction = Transaction.new(from: account, to: account)
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:to_id), "must be other than #{account.id}", "must validate transaction's origin account must be other than its destination account"
-  end
-
-  test "amount must be present" do
-    transaction = Transaction.new
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:amount), "can't be blank", "must validate transaction's amount presence"
-  end
-
-  test "amount must be greater than 0" do
-    transaction = Transaction.new(amount: -420.69)
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:amount), "must be greater than 0", "must validate that transaction's amount value"
-  end
-
-  test "issued_at must be present" do
-    transaction = Transaction.new
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:issued_at), "can't be blank", "must validate transaction's issued_at presence"
-  end
-
-  test "executed_at may not be present" do
-    transaction = Transaction.new(from: accounts(:personal), to: accounts(:expense), amount: 420.69, issued_at: Date.today)
-
-    assert transaction.valid?, "must be valid"
-  end
-
-  test "when executed_at is present, must be equal or later than issued_at" do
-    transaction = Transaction.new(from: accounts(:personal), to: accounts(:expense), amount: 420.69, issued_at: Date.today, executed_at: Date.yesterday)
-
-    assert_not transaction.valid?, "must be invalid"
-    assert_includes transaction.errors.messages_for(:executed_at), "must be greater than or equal to #{Date.today.to_s}", "must validate transaction's executed_at is equal or later than issued_at"
+    assert_equal accounts(:expense).id,
+                 transactions(:personal_expense_0).to.id,
+                 "transaction doesn't go tp expected transaction"
   end
 end
