022ecca63bea79e57c8fe76a0f866294e1ec57ba
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-24 20:39:07 +0100

    adding starting touches for finances_controller.rb

--- app/controllers/bdgtr_controller.rb
+++ app/controllers/bdgtr_controller.rb
@@ -1,4 +1,8 @@
-class BdgtrController < ActionController::Base
+# BdgtrController is the entrypoint for Bdgtr. It only handles request to the application's root route and redirect them
+# to Bdgtr's entrypoint. No controller must inherit from this controller.
+#
+# It only contains a action root, that simply redirects the requests from the application's root to Bdgtr's entrypoint.
+class BdgtrController < ApplicationController
   def root
     redirect_to finances_path
   end
