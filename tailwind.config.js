94e8f4f6c8e6eae52a01ad3ff47b69f48cc371ed
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-25 16:59:43 +0100

    adding theme extension to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/** @type {import('tailwindcss').Config} **/
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        applet: "min-content 1fr min-content"
+      }
+    }
+  }
 }
