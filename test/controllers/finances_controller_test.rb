2372a89f53139a5ce4cac30655c387f4905f461f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-25 17:20:41 +0100

    adding FinancesController tests

--- test/controllers/finances_controller_test.rb
+++ test/controllers/finances_controller_test.rb
@@ -1,8 +1,44 @@
 require "test_helper"
 
 class FinancesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get finances_show_url
-    assert_response :success
+  class ShowAction < ActionDispatch::IntegrationTest
+    test "responses to get with a success" do
+      get finances_path
+
+      assert_response :success
+    end
+
+    test "returns a view that contains a single element with id finances" do
+      get finances_path
+
+      assert_select "#finances", 1
+    end
+
+    test "returns a view that contains a single element with id title" do
+      get finances_path
+
+      assert_select "#title", 1 do
+        assert_select "h1", 1, "Finances"
+        assert_select "p#account-name", 1, ""
+      end
+    end
+
+    test "returns a view that contains a single element with id accounts" do
+      get finances_path
+
+      assert_select "#accounts", 1, ""
+    end
+
+    test "returns a view that contains a single element with id account that is hidden" do
+      get finances_path
+
+      assert_select "#account.hidden", 1, ""
+    end
+
+    test "returns a view that contains a single element with id menu" do
+      get finances_path
+
+      assert_select "#menu", 1, "Here goes the menu"
+    end
   end
 end
