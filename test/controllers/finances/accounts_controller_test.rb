d3c326950c1cee57e1115b6cc56d296fc8cd1a3a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-28 18:19:32 +0100

    fixing Finances::AccountsControllerTest tests for the show action

--- test/controllers/finances/accounts_controller_test.rb
+++ test/controllers/finances/accounts_controller_test.rb
@@ -50,34 +50,32 @@ class Finances::AccountsControllerTest < ActionDispatch::IntegrationTest
 
   class NewAction < ActionDispatch::IntegrationTest
     test "should get new" do
-      get new_finances_account_url
+      get new_finances_account_path
 
       assert_response :success
     end
 
     test "must return a turbo stream response" do
-      get new_finances_account_url
+      get new_finances_account_path
 
       assert_equal "text/vnd.turbo-stream.html; charset=utf-8", response.content_type
     end
 
     class ViewTest < ActionDispatch::IntegrationTest
       test "should return a turbo-stream with action update" do
-        get new_finances_account_url
+        get new_finances_account_path
 
         assert_select "turbo-stream:match('action', ?)", "update", 1
       end
 
       test "should return a turbo-stream with target account" do
-        get new_finances_account_url
+        get new_finances_account_path
 
         assert_select "turbo-stream:match('target', ?)", "account", 1
       end
 
       test "should return a turbo-stream with with a template that contains the account details partial" do
-        get new_finances_account_url
-
-        # assert_equal "", response.body
+        get new_finances_account_path
 
         assert_select "turbo-stream template", 1 do
           assert_select "form:match('action', ?)", finances_accounts_path, 1
@@ -97,11 +95,7 @@ class Finances::AccountsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "should get new" do
-    get new_finances_account_url
 
-    assert_response :success
-  end
 
   test "should create finances_account" do
     assert_difference("Account.count", 1) do
@@ -111,19 +105,72 @@ class Finances::AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to finances_accounts_path
   end
 
-  test "should show finances_account" do
-    get finances_account_url(@account)
-    assert_response :success
+  class ShowAction < ActionDispatch::IntegrationTest
+    setup do
+      @account = accounts(:additional)
+    end
+
+    test "should show account" do
+      get finances_account_path(@account)
+
+      assert_response :success
+    end
+
+    test "must return a turbo stream response" do
+      get finances_account_path(@account)
+
+      assert_equal "text/vnd.turbo-stream.html; charset=utf-8", response.content_type
+    end
+
+    class ViewTest < ActionDispatch::IntegrationTest
+      setup do
+        @account = accounts(:additional)
+      end
+
+      test "should return a turbo-stream with action update" do
+        get finances_account_path(@account)
+
+        assert_select "turbo-stream:match('action', ?)", "update", 1
+      end
+
+      test "should return a turbo-stream with target account" do
+        get finances_account_path(@account)
+
+        assert_select "turbo-stream:match('target', ?)", "account", 1
+      end
+
+      test "should return a turbo-stream with with a template that contains the account details partial" do
+        get finances_account_path(@account)
+
+        assert_select "turbo-stream template", 1 do
+          assert_select "form:match('action', ?)", finances_account_path(@account), 1
+          assert_select "form:match('method', ?)", "post", 1
+
+          assert_select "form", 1 do
+            assert_select "input", 3
+
+            assert_select "input[type='hidden'][name='_method'][value='patch']", 1
+
+            assert_select "input:match('id', ?)", "finances_account_form_name", 1
+            assert_select "input:match('type', ?)", "text", 1
+            assert_select "input:match('value', ?)", @account.name, 1
+
+            assert_select "input:match('class', ?)", "hidden", 1
+            assert_select "input:match('type', ?)", "submit", 1
+          end
+        end
+      end
+    end
   end
 
   test "should update account" do
-    patch finances_account_url(@account), params: { finances_account_form: { name: "On the bounce" } }
+    patch finances_account_path(@account), params: { finances_account_form: { name: "On the bounce" } }
     assert_redirected_to finances_accounts_path
   end
 
   test "should destroy finances_account" do
     assert_difference("Account.count", -1) do
-      delete finances_account_url(@account)
+      delete finances_account_path(@account)
     end
 
     assert_redirected_to finances_accounts_url
