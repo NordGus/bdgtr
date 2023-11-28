9cd0281f66a25bcbb78ec8c2e098c69b530bd322
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-28 11:06:34 +0100

    fixing FinancesController controller test

--- test/controllers/finances_controller_test.rb
+++ test/controllers/finances_controller_test.rb
@@ -13,40 +13,59 @@ class FinancesControllerTest < ActionDispatch::IntegrationTest
         get finances_path
 
         assert_select "#finances", 1
+        assert_select "#finances:match('data-controller', ?)", "finances", 1
+        assert_select "#finances:match('data-action', ?)", "finances--account-saved:success->finances#hideAccount", 1
       end
 
-      test "contains a single element with id title" do
+      test "contains a single toasts container positioned relative to the finances parent" do
         get finances_path
 
-        assert_select "#title", 1 do
-          assert_select "h1", 1
-          assert_select "h1", "Finances"
-          assert_select "a:match('id', ?)", "add_account", 1
-          assert_select "a#add_account", "Add"
+        assert_select "#toasts", "", 1
+        assert_select "#toasts.absolute", 1
+      end
+
+      test "contains a single element with id header" do
+        get finances_path
+
+        assert_select "#header", 1 do
+          assert_select "#title", 1 do
+            assert_select "a.hidden:match('data-finances-target', ?)", "back", 1
+            assert_select "a.hidden:match('data-action', ?)", "click->finances#hideAccount", 1
+            assert_select "h1", "Finances", 1
+          end
+
+          assert_select "a:match('data-turbo-frame', ?)", "account", 2
+
+          assert_select "a:match('data-finances-target', ?)", "new", 1
+          assert_select "a:match('data-action', ?)", "click->finances#displayAccount", 1
+          assert_select "a:match('href', ?)", new_finances_account_path, 1
+          assert_select "a", "Add", 1
+
+          assert_select "a:match('data-finances-target', ?)", "save", 1
+          assert_select "a:match('data-action', ?)", "click->finances#saveAccount", 1
+          assert_select "a:match('data-turbo', ?)", "true", 1
+          assert_select "a", "Save", 1
         end
       end
 
-      test "contains a single element with id accounts" do
+      test "contains a single turbo-frame with id accounts" do
         get finances_path
 
-        assert_select "#accounts", 1
-        assert_select "#accounts", ""
+        assert_select "turbo-frame#accounts", "", 1
       end
 
-      test "contains a single hidden element with id account" do
+      test "contains a single hidden turbo-frame with id account" do
         get finances_path
 
-        assert_select "#account", 1
-        assert_select "#account", ""
-        assert_select "#account.hidden", 1, ""
-        assert_select "#account.hidden", ""
+        assert_select "turbo-frame#account.hidden", "", 1
       end
 
       test "contains a single element with id menu" do
         get finances_path
 
-        assert_select "#menu", 1
-        assert_select "#menu", "Here goes the menu"
+        assert_select "#menu", 1 do
+          assert_select "a", "Finances", 1
+        end
       end
     end
   end
