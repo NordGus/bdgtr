47b567488cb4605e91f778634c3be6f4f516df84
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-29 16:28:12 +0100

    updating Finances::AccountsController::ShowActionTest

--- test/controllers/finances/accounts_controller/show_action_test.rb
+++ test/controllers/finances/accounts_controller/show_action_test.rb
@@ -22,39 +22,55 @@ class Finances::AccountsController::ShowActionTest < ActionDispatch::Integration
       @account = accounts(:additional)
     end
 
-    test "should return a turbo-stream with action update" do
+    test "should return the expected amount of turbo-streams" do
       get finances_account_path(@account)
 
-      assert_select "turbo-stream:match('action', ?)", "update", 1
+      assert_select "turbo-stream", 2
     end
 
-    test "should return a turbo-stream with target account" do
-      get finances_account_path(@account)
+    class AccountTurboStream < ViewTest
+      test "should return on turbo-stream that updates the account turbo-frame" do
+        get finances_account_path(@account)
 
-      assert_select "turbo-stream:match('target', ?)", "account", 1
-    end
+        assert_select "turbo-stream[action='update'][target='account']", 1
+      end
 
-    test "should return a turbo-stream with with a template that contains the account details partial" do
-      get finances_account_path(@account)
+      test "should return the expected template inside the turbo-stream" do
+        get finances_account_path(@account)
 
-      assert_select "turbo-stream template", 1 do
-        assert_select "form:match('action', ?)", finances_account_path(@account), 1
-        assert_select "form:match('method', ?)", "post", 1
+        assert_select "turbo-stream[action='update'][target='account'] template", 1 do
+          assert_select "form:match('action', ?)", finances_account_path(@account), 1
+          assert_select "form:match('method', ?)", "post", 1
 
-        assert_select "form", 1 do
-          assert_select "input", 3
+          assert_select "form", 1 do
+            assert_select "input:match('class', ?)", "hidden", 1
+            assert_select "input:match('type', ?)", "submit", 1
+          end
 
-          assert_select "input[type='hidden'][name='_method'][value='patch']", 1
-
-          assert_select "input:match('id', ?)", "finances_account_form_name", 1
-          assert_select "input:match('type', ?)", "text", 1
-          assert_select "input:match('value', ?)", @account.name, 1
-
-          assert_select "input:match('class', ?)", "hidden", 1
-          assert_select "input:match('type', ?)", "submit", 1
+          assert_select "a[data-action='click->modal#open']", "Delete Account", 1
         end
+      end
+    end
 
-        assert_select "a[href='#{finances_account_path(@account)}'][data-turbo-method='delete'][data-turbo-frame='account']", 1
+    class ModalBodyTurboStream < ViewTest
+      test "should return on turbo-stream that updates the account turbo-frame" do
+        get finances_account_path(@account)
+
+        assert_select "turbo-stream[action='update'][target='modal_body']", 1
+      end
+
+      test "should return the expected template inside the turbo-stream" do
+        get finances_account_path(@account)
+
+        assert_select "turbo-stream[action='update'][target='modal_body'] template", 1 do
+          assert_select(
+            "a[href='#{finances_account_path(@account)}'][data-turbo-method='delete'][data-turbo-frame='account'][data-action='click->modal#close']",
+            "Yes",
+            1
+          )
+
+          assert_select "a[data-action='click->modal#close']", "No", 1
+        end
       end
     end
   end
