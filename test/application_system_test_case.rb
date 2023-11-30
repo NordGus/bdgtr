5e8ef43d18e801a32745cc631df878f5a2e2ad28
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-11-30 18:57:25 +0100

    limiting the number of parallel workers for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  parallelize workers: 2
+
   driven_by :selenium, using: :headless_firefox, screen_size: [1920, 1080]
 end
