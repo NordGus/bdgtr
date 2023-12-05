e379d610e2bc3ecaf5ab17288eac8adb06654382
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-12-05 17:02:16 +0100

    redesigning menu items with placeholders for icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
   def menu_item_classes(item_controller:, current_controller:)
+    base = "flex flex-col justify-center items-center flex-1 py-2 px-3 rounded-lg text-xs"
+
     if item_controller == current_controller
-      "block flex-1 py-2 px-3 text-center rounded-lg bg-neutral-300 dark:bg-neutral-700 underline"
+      "#{base} bg-neutral-300 dark:bg-neutral-700 underline"
     else
-      "block flex-1 py-2 px-3 text-center rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700"
+      "#{base} hover:bg-neutral-300 hover:dark:bg-neutral-700"
     end
   end
 end
